2df93ccf90795adeb714ee927ab16606196f20e7
Zoullien <70811914+juls5528@users.noreply.github.com>
2022-08-10 12:46:45 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,84 @@
+const Discord = require("discord.js")   
+const env = require("dotenv")
+const bot = new Discord.Client({
+    intents:[
+        Discord.Intents.FLAGS.GUILDS,
+        Discord.Intents.FLAGS.GUILD_MESSAGES,
+        Discord.Intents.FLAGS.GUILD_BANS,
+        Discord.Intents.FLAGS.GUILD_MEMBERS, 
+        Discord.Intents.FLAGS.GUILD_VOICE_STATES
+         
+    ]
+})
+bot.on("ready", bot   => {
+    console.log(`Im ready to do nothing for a wile`)
+
+    bot.user.setActivity("Whats a Admin Launch ?.", {
+        type: "STREAMING",
+        url: "https://www.twitch.tv/monstercat"
+      });
+
+});
+
+
+
+bot.on("messageCreate", message =>{
+    if(message.content === `./terminal`){
+        
+        const TerminalHome = new Discord.MessageEmbed()
+        .setDescription("```Terminal Hook [version 0.1\
+                                      (c) Zoullien Ideas.Tous droits réservés\
+         \
+         \
+         \
+         C:```")
+       .setColor("#2e3136")
+       
+        message.channel.send({ embeds: [TerminalHome]})
+    
+        
+    }
+})
+bot.on("messageCreate", message =>{
+    if(message.content === `./thelp`){
+        
+        const TerminalHome = new Discord.MessageEmbed()
+        .setDescription("```Terminal Hook [version 0.1]\
+                                      (c) Zoullien Ideas.Tous droits réservés\
+         \
+         \
+         \
+         C:help\
+         \
+         \
+                                                                  BANㅤㅤㅤㅤPermet de Bannir des personne dérangent le serveur tout en detruisant les règles.\
+\
+\
+                                C:```")
+       .setColor("#2e3136")
+       
+        message.channel.send({ embeds: [TerminalHome]})
+    
+        
+    }
+})
+
+bot.on("messageCreate", message =>{
+    if(message.content === `./tban`){
+        
+        const TerminalHome = new Discord.MessageEmbed()
+        .setDescription("```Terminal Hook [version 0.1\
+                                      (c) Zoullien Ideas.Tous droits réservés\
+         \
+         \
+         \
+         C:ban```")
+       .setColor("#2e3136")
+       
+        message.channel.send({ embeds: [TerminalHome]})
+    
+        
+    }
+})
+
+bot.login("MTAwNjg1OTAxNjUwMjI1NTY0Ng.GE3sQf.aKsMbdol_NKiZYO32EL6bA1Jv6SXMUI49x75HY")
